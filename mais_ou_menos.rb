eaf292655b1b43ab7f6fee6f6c023a96eb768363
Gabriel Minatto <gabriel.minatto@aulalivre.net>
2017-05-11 13:10:11 -0300

    aula 2

--- mais_ou_menos.rb
+++ mais_ou_menos.rb
@@ -8,10 +8,10 @@ puts "Começaremos o jogo para você, "+nome
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido...que tal adivinhar hoje o nosso número secreto?"
-tentativas = 5
-for tentativa in 1..tentativas do
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas do
     puts "\n\n\n\n"
-   puts "Tentativa "+tentativa.to_s+" de "+ tentativas.to_s
+   puts "Tentativa "+tentativa.to_s+" de "+ limite_de_tentativas.to_s
     puts "Entre com o número"
     chute = gets
     puts "Será que acertou? Você chutou "+chute
@@ -20,6 +20,7 @@ for tentativa in 1..tentativas do
 
     if acertou
         puts "Acertou!"
+        break
     else
         maior = numero_secreto > chute.to_i
         if maior
