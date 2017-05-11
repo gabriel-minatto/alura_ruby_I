4cbd3f2864e0df909db462e6d32f8dc2f99c225c
Gabriel Minatto <gabriel.minatto@aulalivre.net>
2017-05-11 15:23:42 -0300

    aula 3

--- mais_ou_menos.rb
+++ mais_ou_menos.rb
@@ -1,34 +1,55 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
+def da_boas_vindas
+    puts "Bem vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, "+nome
+end
 
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, "+nome
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "Escolhido...que tal adivinhar hoje o nosso número secreto?"
+    sorteado
+end
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido...que tal adivinhar hoje o nosso número secreto?"
-limite_de_tentativas = 5
-for tentativa in 1..limite_de_tentativas do
+def pede_um_numero(tentativa,limite_de_tentativas)
     puts "\n\n\n\n"
-   puts "Tentativa "+tentativa.to_s+" de "+ limite_de_tentativas.to_s
+    puts "Tentativa "+tentativa.to_s+" de "+ limite_de_tentativas.to_s
     puts "Entre com o número"
     chute = gets
     puts "Será que acertou? Você chutou "+chute
+    chute.to_i
+end
 
-    acertou = numero_secreto == chute.to_i
+def verifica_se_acertou(numero_secreto,chute)
+    acertou = numero_secreto == chute
 
     if acertou
         puts "Acertou!"
-        break
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior"
-        else
-            puts "O número secreto é menor"
-        end
+        return acertou
     end
+
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O número secreto é maior"
+    else
+        puts "O número secreto é menor"
+    end
+    false #return false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas do
+    chute = pede_um_numero tentativa, limite_de_tentativas #pede_um_numero(tentativa, limite_de_tentativas)
+    if verifica_se_acertou numero_secreto, chute
+        break
+    end
+    #break if verifica_se_acertou numero_secreto, chute
 end
 =begin
  comentario de varias linhas
