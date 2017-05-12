fd5cfdcc47f16032e73a8bb4cf9abfd186e9c811
Gabriel Minatto <gabriel.minatto@aulalivre.net>
2017-05-12 13:08:44 -0300

    Aula 4

--- mais_ou_menos.rb
+++ mais_ou_menos.rb
@@ -1,9 +1,10 @@
 def da_boas_vindas
     puts "Bem vindo ao jogo da adivinhação"
     puts "Qual é o seu nome?"
-    nome = gets
+    nome = gets.strip
     puts "\n\n\n\n\n\n"
-    puts "Começaremos o jogo para você, "+nome
+    #puts "Começaremos o jogo para você, "+nome
+    puts "Começaremos o jogo para você, #{nome}"
 end
 
 def sorteia_numero_secreto
@@ -13,13 +14,20 @@ def sorteia_numero_secreto
     sorteado
 end
 
-def pede_um_numero(tentativa,limite_de_tentativas)
+def pede_um_numero(chutes,tentativa,limite_de_tentativas)
     puts "\n\n\n\n"
-    puts "Tentativa "+tentativa.to_s+" de "+ limite_de_tentativas.to_s
+    #puts "Tentativa "+tentativa.to_s+" de "+ limite_de_tentativas.to_s
+    puts "Tentativa #{tentativa} de #{limite_de_tentativas}" #interpolação
+    #for cont in 0..tentativa-2 do
+        #puts "Chutes até agora "+chutes[cont].to_s
+    #end
+    #puts "Chutes até agora "+chutes.to_s
+    puts "Chutes até agora #{chutes.to_s}"
     puts "Entre com o número"
-    chute = gets
-    puts "Será que acertou? Você chutou "+chute
-    chute.to_i
+    chute = gets.strip
+    #puts "Será que acertou? Você chutou "+chute
+    puts "Será que acertou? Você chutou #{chute}"
+    chute.to_i #return
 end
 
 def verifica_se_acertou(numero_secreto,chute)
@@ -43,9 +51,14 @@ da_boas_vindas
 numero_secreto = sorteia_numero_secreto
 
 limite_de_tentativas = 5
+chutes = []
 
 for tentativa in 1..limite_de_tentativas do
-    chute = pede_um_numero tentativa, limite_de_tentativas #pede_um_numero(tentativa, limite_de_tentativas)
+
+    chute = pede_um_numero chutes,tentativa, limite_de_tentativas #pede_um_numero(tentativa, limite_de_tentativas)
+    chutes << chute #o << coloca na ultima
+    #chutes[chutes.size] = chute
+    #chutes[tentativa - 1] = chute
     if verifica_se_acertou numero_secreto, chute
         break
     end
